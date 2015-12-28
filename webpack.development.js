eaa87b0b26b80dcdbc7d5fd4402f89239b0b498b
Jaroslaw Rzeszotko <jrzeszotko@gmail.com>
2015-12-28 10:28:46 +0100

    Build improvements.

---
+++ webpack.development.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+var reactExternal = {
+  root: 'React',
+  commonjs2: 'react',
+  commonjs: 'react',
+  amd: 'react'
+};
+
+module.exports = {
+  externals: {
+    'react': reactExternal
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+    ]
+  },
+  output: {
+    library: 'ReactFormLogic',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['', '.js']
+  }
+};
